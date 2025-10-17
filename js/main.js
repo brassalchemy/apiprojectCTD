f8189069576d45f5a1e4717f433811421e333209
jpgcandykane <kendramensah4@gmail.com>
2025-10-16 23:45:47 -0400

    Updated html and fetch call

--- js/main.js
+++ js/main.js
@@ -1,22 +1,10 @@
-async function fetchIndependentData() { 
-    try {
-        const firstPromise = await fetch('https://api.artic.edu/api/v1/artworks/275405')
-        const secondPromise = await fetch('https://api.artic.edu/api/v1/artworks/129884')
-        const [firstPhoto, secondPhoto] = await Promise.all([firstPromise, secondPromise])
-        console.log(firstPhoto, secondPhoto)    
-    } catch(error){ 
-        console.log("Uh oh something went wrong", + error)
-    }
-}
-
-// https://api.artic.edu/api/v1/artworks/search?q=ghana&query[term][is_public_domain]=true&fields=artist_title,date_display,image_id,thumbnail,title
-
-// fetch("https://api.artic.edu/api/v1/artworks") 
-   //  .then(response => response.json()) // capture response and turn into json format
+// fetch("https://api.artic.edu/api/v1/artworks/search?q=editorial&query[term][is_public_domain]=true&fields=artist_title,date_display,image_id,thumbnail,title")
+    // .then(response => response.json()) 
    //  .then(data => { 
-       //  const myImages = data;
-       //  console.log(myImages);
-   //  })
-    // .catch(error => console.error('Error', error))
-    
+        // const myFirstImage = (data)
+       //  console.log(myFirstImage)
+  //   }) 
+
+
+
 
